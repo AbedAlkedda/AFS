220a5ba8867dfa3097baf3fa8852bea336e73887
abed <zagdkde@gmail.com>
2023-05-27 19:10:50 +0200

    small fixes

--- afs.rb
+++ afs.rb
@@ -5,17 +5,17 @@ require_relative 'automata'
 
 require 'byebug'
 
-# d_a    = [[1, 2], [2, 5], [3, 1], [4, 3], [5, 4]]
-# d_b    = [[2, 4], [3, 3], [4, 4], [5, 1], [5, 3]]
-# states = [1, 2, 3, 4, 5]
-# starts = [1]
-# finals = [5]
-
-d_a    = [[1, 1], [2, 3], [3, 2]]
-d_b    = [[1, 2], [2, 1], [3, 3]]
-states = [1, 2, 3]
+d_a    = [[1, 2], [2, 5], [3, 1], [4, 3], [5, 4]]
+d_b    = [[2, 4], [3, 3], [4, 4], [5, 1], [5, 3]]
+states = [1, 2, 3, 4, 5]
 starts = [1]
-finals = [1]
+finals = [5]
+
+# d_a    = [[1, 1], [2, 3], [3, 2]]
+# d_b    = [[1, 2], [2, 1], [3, 3]]
+# states = [1, 2, 3]
+# starts = [1]
+# finals = [1]
 
 # d_a    = [[1, 1], [2, 2]]
 # d_b    = [[1, 2], [2, 1]]
@@ -26,5 +26,6 @@ finals = [1]
 
 NFA.new do |a|
   a.build d_a, d_b, states, starts, finals
-  a.nfa_to_reg
+  # a.nfa_to_reg
+  a.to_dfa
 end
--- automata.rb
+++ automata.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require_relative 'rx'
 
 # Practice some stuff out Theorithsche Informatik
