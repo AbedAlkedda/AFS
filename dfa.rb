0b0ce25ed77808b35dca5226be9020600fa2d9f9
abed <zagdkde@gmail.com>
2023-05-28 21:10:26 +0200

    print all ri fix

--- dfa.rb
+++ dfa.rb
@@ -17,7 +17,7 @@ class DFA < Automata
     steps       = _steps_builder finals, states, steps
 
     _print finals_hash, r0, finals, states
-    _print steps
+    _print_steps steps
   end
 
   private
@@ -97,7 +97,7 @@ class DFA < Automata
     puts "{F, Q\\F }={#{f_equivalent}}, {#{q_without_f}}"
   end
 
-  def _print(steps)
+  def _print_steps(steps)
     steps.each { |k, v| puts "#{k}=#{v}" }
   end
 
