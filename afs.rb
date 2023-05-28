c848bad17a298fa014f6ce936738ac6b447efc9a
abed <zagdkde@gmail.com>
2023-05-28 20:52:56 +0200

    new example added

--- afs.rb
+++ afs.rb
@@ -6,21 +6,21 @@ require_relative 'automata'
 require 'awesome_print'
 require 'byebug'
 
-# d_a    = [[1, 1], [2, 3], [3, 2]]
-# d_b    = [[1, 2], [2, 1], [3, 3]]
-# states = [1, 2, 3]
-# starts = [1]
-# finals = [1]
-
 d_a    = [[1, 1], [2, 2], [2, 1]]
 d_b    = [[1, 2]]
 states = [1, 2]
 starts = [1]
 finals = [2]
 
+# d_a    = [[1, 1], [2, 1]]
+# d_b    = [[1, 2]]
+# states = [1, 2]
+# starts = [1]
+# finals = [2]
+
 NFA.new do |a|
   a.build d_a, d_b, states, starts, finals
-  puts 'crossboss'
+  puts 'crossboss:'
   a.potens_set
   puts
   dfa = a.to_dfa
