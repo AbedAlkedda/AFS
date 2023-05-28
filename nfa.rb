977e61813d93bf0b45b017357259b1e27544862e
abed <zagdkde@gmail.com>
2023-05-28 13:21:18 +0200

    dfa print text changed

--- nfa.rb
+++ nfa.rb
@@ -8,7 +8,7 @@ class NFA < Automata
   def to_dfa
     dfa = nfa_to_dfa
 
-    puts "nfa:\n\nfinals: #{dfa.finals.map { |ele| _concat(ele).to_i }}"
+    puts "dfa:\n\nfinals: #{dfa.finals.map { |ele| _concat(ele).to_i }}"
     puts "states: #{dfa.states.map(&:to_i)}"
     dfa.delta_star.each { |k, v| v.each { |e| puts "(#{e[0]} ,'#{k}', #{e[1]})" } }
 
