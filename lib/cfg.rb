65059c28bdd0b5b58fe19d8fed8effbe00b0f692
abed <zagdkde@gmail.com>
2023-06-16 01:27:02 +0200

    cfg: input simplifed

--- lib/cfg.rb
+++ lib/cfg.rb
@@ -1,31 +1,40 @@
 class CFG
-  attr_accessor :terminals, :variables, :start_symbol, :productions
+  attr_accessor :alphabet, :vars, :start_var, :rules
+  attr_reader   :rnd_words
+  attr_writer   :lang
 
-  def initialize(terminals, variables, start_symbol)
-    @terminals = terminals
-    @variables = variables
-    @start_symbol = start_symbol
-    @productions = {}
-  end
-
-  def add_production(lhs, rhs)
-    @productions[lhs] ||= []
-    @productions[lhs] << rhs
+  def initialize(alphabet, vars, start_var, rule)
+    @alphabet  = alphabet
+    @vars      = vars
+    @start_var = start_var
+    @rules     = _rules rule
+    @rnd_words = []
   end
 
   def generate_random
-    expand(@start_symbol)
+    word = _expand @start_var
+
+    # return unless _in_lang? word
+
+    @rnd_words << word unless @rnd_words.include? word
   end
 
   private
 
-  def expand(symbol)
-    production = @productions[symbol]
-    if production.nil?
-      return symbol
-    else
-      rhs = production.sample
-      rhs.map { |s| expand(s) }.join
-    end
+  def _expand(symbol)
+    production = @rules[symbol]
+
+    return symbol if production.nil?
+
+    rhs = production.sample
+    rhs.map { |s| _expand(s) }.join
   end
+
+  def _rules(rules)
+    rules.each_value { |k| k.each_with_index { |item, index| k[index] = item[0].is_a?(String) ? item[0].split('') : item } }
+  end
+
+  def _in_lang?(word)
+  end
+
 end
