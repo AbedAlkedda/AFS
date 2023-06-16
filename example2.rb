de0e0e988cd241971599f28c05144337b134f802
abed <zagdkde@gmail.com>
2023-06-16 02:57:16 +0200

    cfg_check_succ: lambda fix and duplicate remove

--- example2.rb
+++ example2.rb
@@ -8,10 +8,11 @@ rules = { 'S' => [[]] }
 rules['S'] << ['aSc']
 rules['S'] << ['bSc']
 
-cfg = Falafel.new {}.cfg alphabet, vars_set, start_var, rules
+falafel = Falafel.new {}
+cfg     = falafel.cfg alphabet, vars_set, start_var, rules
 
 # {a^x b^y c^x+y | x, y ∈ N}
-reg2 = ->(w) { (w.count('a') + w.count('b') == w.count('c')) && ( w.match(/\Aa*b*c*\z/)) }
+reg2 = ->(w) { (w.count('a') + w.count('b') == w.count('c')) && ( w.match(/\A(a*b*c*)\z/)) }
 
 cfg.lang = reg2
 
