d0e2e2c6c71c025c92ba569428130dbd820997e2
abed <zagdkde@gmail.com>
2023-07-27 23:40:49 +0200

    cyk_fix: epsilon free function added

--- lib/cfg.rb
+++ lib/cfg.rb
@@ -5,7 +5,7 @@
 # generates random words
 class CFG
   attr_accessor :start_var, :rules
-  attr_reader   :rnd_words, :reachables
+  attr_reader   :rnd_words, :reachables, :rules_ef
 
   def initialize(alphabet, vars, start_var, rules)
     @start_var  = start_var
@@ -13,9 +13,9 @@ class CFG
     @rnd_words  = []
     @reachables = []
     @vars       = vars
+    @rules_ef   = {} # Rules Epsilon free
     # @alphabet   = alphabet
     # @chomsky_nf = {}
-    # @rules_ef   = {} # Rules Epsilon free
 
     # Cyk
     # @letter     = 'H'
@@ -49,35 +49,19 @@ class CFG
     ''
   end
 
-  # def epsilon_clear
-  #   grammer = []
+  def epsilon_clear
+    epsilon = _epsilon
 
-  #   @rules.each do |_, rule|
-  #     return false unless rule.include? []
+    return if epsilon.empty?
 
-  #     s_rule = ''
-  #     rule.select { |x| x unless x.empty? }.each do |sub_rule|
-  #       s_rule = sub_rule.join ''
+    epsilon.each do |e_class|
+      cleared_rules     = _cleared_rules @rules[e_class]
+      epsilon_free_word = cleared_rules.join ''
+      grammer           = _grammer_epsilon_free epsilon_free_word, cleared_rules, e_class
 
-  #       (_s_index s_rule).each do |cond|
-  #         grammer << s_rule if cond.empty?
-
-  #         if cond.size == 1
-  #           new_rule = sub_rule.join ''
-  #           new_rule.slice!(cond[0])
-  #           grammer << new_rule
-  #         end
-
-  #         next unless cond.size >= 2
-
-  #         holder = sub_rule.clone
-  #         cond.reverse.map { |i| holder.delete_at i }
-  #         grammer << holder.join('')
-  #       end
-  #     end
-  #   end
-  #   @rules_ef['S'] = grammer
-  # end
+      @rules_ef[e_class] = grammer
+    end
+  end
 
   # def chomsky_run(rules)
   #   @res = {}
@@ -252,20 +236,56 @@ class CFG
   #   val
   # end
 
-  # def _s_index(s_rule)
-  #   s_index = []
-  #   s_rule.each_char.with_index { |char, index| s_index << index if char == 'S' }
+  # epsilon free start
+  def _build_possibilities(s_rule)
+    s_index = []
+    s_rule.each_char.with_index { |char, index| s_index << index if char == 'S' }
 
-  #   _power_set s_index
-  # end
+    _power_set s_index
+  end
 
-  # def _power_set(set)
-  #   if set.empty?
-  #     [[]]
-  #   else
-  #     element = set[0]
-  #     subsets = _power_set set[1..]
-  #     subsets + subsets.map { |subset| [element] + subset }
-  #   end
-  # end
+  def _power_set(set)
+    if set.empty?
+      [[]]
+    else
+      element = set[0]
+      subsets = _power_set set[1..]
+      subsets + subsets.map { |subset| [element] + subset }
+    end
+  end
+
+  def _epsilon
+    res = @rules.map do |var, rules|
+      next unless rules.include? []
+
+      var
+    end
+    res.reject(&:nil?)
+  end
+
+  def _cleared_rules(rules)
+    rules.reject(&:empty?)
+  end
+
+  def _remove_e_class(possibility, cleared_rules)
+    return cleared_rules.join '' if possibility.empty?
+
+    new_rule = cleared_rules.join ''
+    possibility.reverse.each { |index| new_rule.slice! index }
+
+    new_rule
+  end
+
+  def _grammer_epsilon_free(epsilon_free_word, cleared_rules, e_class)
+    possibilities = _build_possibilities epsilon_free_word
+
+    res = possibilities.map do |possibility|
+      _remove_e_class possibility, cleared_rules
+    end
+
+    res << epsilon_free_word.delete(e_class) if possibilities.size == 1
+
+    res
+  end
+  # epsilon free end
 end
