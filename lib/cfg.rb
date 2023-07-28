d804277fdb45a512bc51bebc726aac877294c029
abed <zagdkde@gmail.com>
2023-07-28 17:25:07 +0200

    cyk_fix: chainig free moved to a class and cfg cleared

--- lib/cfg.rb
+++ lib/cfg.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'epsilon_free'
+require_relative 'chaining_free'
 
 # Samll CFG impletation
 # Generate words and check if words are the @lang
@@ -57,11 +58,7 @@ class CFG
   end
 
   def chaining_free
-    c_r = _chaining_relation
-
-    # build transivity relation from _chaining_relation(K) as A
-    # remove _chaining_relation from rules
-    @rules_cf = (_rebuild_rules - c_r) + c_r.map { |r| _transitivity_relation r }.reduce(:concat)
+    @rules_cf = ChainingFree.new.run @rules, @vars
   end
   # def chomsky_run(rules)
   #   @res = {}
@@ -82,52 +79,6 @@ class CFG
   # end
 
   private
-
-  # chaining_free start
-  def _chaining_relation
-    rules = _rebuild_rules
-
-    rules & @vars.product(@vars)
-  end
-
-  def _rebuild_rules
-    result = []
-    @rules.each do |var, rule|
-      rule.map do |r|
-        result << [var, r.join]
-      end
-    end
-
-    result
-  end
-
-  def _transitivity_relation(pair)
-    transitive_relation = [pair]
-    loop do
-      new_pairs = []
-
-      _rebuild_rules.each do |a, b|
-        next unless pair[1] == a && !transitive_relation.include?([pair[0], b])
-
-        new_pairs << [pair[0], b]
-      end
-
-      break if new_pairs.empty?
-
-      transitive_relation.concat(new_pairs)
-    end
-
-    transitive_relation
-  end
-
-  def _new_pairs(rules)
-    rules.each do |a, b|
-      next unless pair[1] == a && !transitive_relation.include?([pair[0], b])
-
-      new_pairs << [pair[0], b]
-    end
-  end
-  # chaining_free end
 
   def _rules(rules)
     rules.each_value { |k| k.each_with_index { |item, index| k[index] = item[0].is_a?(String) ? item[0].split('') : item } }
--- lib/chaining_free.rb
+++ lib/chaining_free.rb
@@ -1,4 +1,60 @@
 # frozen_string_literal: true
 
+# remove chaining rules from CFG
 class ChainingFree
+  def run(rules, vars)
+    @rules = rules
+    @vars  = vars
+    c_r    = _chaining_relation
+
+    # build transivity relation from _chaining_relation(K) as A
+    # remove _chaining_relation from rules
+    (_rebuild_rules - c_r) + c_r.map { |r| _transitivity_relation r }.reduce(:concat)
+  end
+
+  private
+
+  def _chaining_relation
+    rules = _rebuild_rules
+
+    rules & @vars.product(@vars)
+  end
+
+  def _rebuild_rules
+    result = []
+    @rules.each do |var, rule|
+      rule.map do |r|
+        result << [var, r.join]
+      end
+    end
+
+    result
+  end
+
+  def _transitivity_relation(pair)
+    transitive_relation = [pair]
+    loop do
+      new_pairs = []
+
+      _rebuild_rules.each do |a, b|
+        next unless pair[1] == a && !transitive_relation.include?([pair[0], b])
+
+        new_pairs << [pair[0], b]
+      end
+
+      break if new_pairs.empty?
+
+      transitive_relation.concat(new_pairs)
+    end
+
+    transitive_relation
+  end
+
+  def _new_pairs(rules)
+    rules.each do |a, b|
+      next unless pair[1] == a && !transitive_relation.include?([pair[0], b])
+
+      new_pairs << [pair[0], b]
+    end
+  end
 end
