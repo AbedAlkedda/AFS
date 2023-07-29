a7afec081c7c1c88706c0dd4ff2be8c0133f9c65
abed <zagdkde@gmail.com>
2023-07-29 22:50:29 +0200

    cyk_fix: chomsky function added to a class

--- lib/cfg.rb
+++ lib/cfg.rb
@@ -2,6 +2,7 @@
 
 require_relative 'epsilon_free'
 require_relative 'chaining_free'
+require_relative 'chomsky_nf'
 
 # Samll CFG impletation
 # Generate words and check if words are the @lang
@@ -63,17 +64,9 @@ class CFG
   end
 
   def chomksy_nf
-    # generate new vars 'a ∈ Σ und Regeln (va, a)'
-    new_rules = {}
-    @rules    = @rules_ef || @rules_cf
+    chomksy = ChomskyNF.new
 
-    # add cases like (A, a), (B, b)
-    _add_single_vars new_rules
-
-    # change rules like (X, aX) to (X, AX)
-    _handle_simple_rule new_rules
-
-    @chomksy_nf_rules = _handle_all_rule new_rules
+    @chomksy_nf_rules = chomksy.run @rules_ef, @alphabet
   end
 
   # def cyk_run(word)
@@ -87,131 +80,6 @@ class CFG
   # end
 
   private
-
-  # chomksy_nf start
-  def _add_single_vars(new_rules)
-    @rules.values.reduce(:concat).each do |rule|
-      rule.select { |var| var == var.downcase }.each do |r|
-        new_rules[r.upcase] = r
-      end
-    end
-  end
-
-  def _handle_simple_rule(new_rules)
-    @rules.each do |var, rule|
-      new_rules[var] = []
-      rule.each do |r|
-        next if r.empty?
-
-        new_rules[var] << r.map(&:upcase)
-      end
-    end
-  end
-
-  def _handle_all_rule(new_rules)
-    new_rules_buffer = {}
-
-    new_rules.each do |var, rules|
-      next if _is_character? rules
-
-      new_rules_buffer = _new_rules_buffer rules, var
-    end
-
-    new_rules.merge! new_rules_buffer
-  end
-
-  def _is_character?(value)
-    value.is_a?(String) && value.length == 1
-  end
-
-  def _new_rules_buffer(rules, var)
-    helper_vars = _helper_vars rules
-    helper_vars.merge! @alphabet.each_with_object({}) { |ele, meme| meme[ele.capitalize] = ele.capitalize }
-
-    _new_rules rules, var, helper_vars
-  end
-
-  def _new_rules(rules, var, helper_vars)
-    holder = {}
-
-    rules.each do |rule|
-      rest = ''
-      rule.each_with_index do |head, index|
-        current_var = index.zero? ? var : rest
-
-        holder[current_var] ||= []
-
-        rest = _chomsky_nf_vars index, rule, helper_vars
-
-        nf_rest = "#{head}#{rest}"
-
-        if index + 2 == rule.size
-          if rest.nil?
-            rest     = rule.last
-            nf_rest += rest
-          end
-
-          holder[current_var] << nf_rest
-
-          break
-        else
-          holder[current_var] << nf_rest
-        end
-      end
-    end
-
-    holder
-  end
-
-  def _chomsky_nf_vars(index, rule, helper_vars)
-    rule_size = rule.size
-    rest      = rule[(index + 1)..rule_size].join
-
-    helper_vars.key(rest)
-  end
-
-  def _helper_vars(rules)
-    helper_vars = {}
-
-    rules.each do |rule|
-      rule.each_with_index do |_, index|
-        letter, rest = _chomsky_nf_helper_vars index, rule, helper_vars.keys
-
-        break if index + 2 == rule.size
-
-        next if helper_vars.values.include? rest
-
-        helper_vars[letter] = rest unless helper_vars.key?(letter) && rest.size > 1
-      end
-    end
-
-    helper_vars
-  end
-
-  def _chomsky_nf_helper_vars(index, rule, helper_vars)
-    rule_size   = rule.size
-    letter      = helper_vars.empty? ? _find_letter(index) : helper_vars.sort!.last.succ
-    rest        = rule[(index + 1)..rule_size].join
-
-    [letter, rest]
-  end
-
-  def _find_letter(n)
-    result = 'H'
-    n.times { result = result.succ }
-
-    result
-  end
-
-  def _add_var(holder, key, value)
-    if holder.key? key
-      holder[key] << [value]
-    else
-      holder[key] = [value]
-    end
-  end
-
-  # chomksy_nf end
 
   def _rules(rules)
     rules.each_value { |k| k.each_with_index { |item, index| k[index] = item[0].is_a?(String) ? item[0].split('') : item } }
