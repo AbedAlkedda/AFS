cec7a86946aa1e328b81afa1248c23742154f645
abed <zagdkde@gmail.com>
2023-08-02 23:19:12 +0200

    modify_chomksy_nf_algo: test added

--- spec/cfg_spec.rb
+++ spec/cfg_spec.rb
@@ -2,8 +2,8 @@
 
 require_relative '../lib/falafel'
 
-RSpec.describe 'Calculator integration' do
-  it 'Generate Chomksy normal form' do
+RSpec.describe 'Chomsky with epsilon' do
+  it 'Check Chomksy normal form without epsilon' do
     alphabet  = %w[a b]
     vars_set  = %w[S X Y]
     start_var = 'S'
@@ -11,11 +11,35 @@ RSpec.describe 'Calculator integration' do
 
     falafel = Falafel.new {}
     cfg     = falafel.cfg alphabet, vars_set, start_var, rules
+
     cfg.epsilon_free
     cfg.chomksy_nf cfg.rules_ef
+
     result = cfg.chomksy_nf cfg.rules_ef
+
     res = { 'A' => 'a', 'B' => 'b', 'S' => %w[AH AJ AI AB], 'H' => ['SI'], 'I' => ['BS'], 'J' => ['SB'] }
 
     expect(result).to eq(res)
+  end
+end
+
+RSpec.describe 'CYK implementing' do
+  it 'CYK word problem' do
+    alphabet  = %w[a b]
+    vars_set  = %w[S X Y]
+    start_var = 'S'
+    rules     = { 'S' => [['b'], ['a'], ['aSS']] }
+
+    falafel = Falafel.new {}
+    cfg     = falafel.cfg alphabet, vars_set, start_var, rules
+
+    cfg.chomksy_nf cfg.rules_ef
+
+    check_results = { 'abb' => true, 'aaa' => true, 'aab' => true }
+    check_results.each do |word, res|
+      cfg.cyk_run word
+      result = cfg.is_in_l
+      expect(result).to eq(res)
+    end
   end
 end
