cae0b6f0f80656253291ec1d07c56d9ce2295029
abed <zagdkde@gmail.com>
2023-08-11 19:33:07 +0200

    fix_chomsky_nf_simplifier: spec modified

--- spec/cfg_spec.rb
+++ spec/cfg_spec.rb
@@ -2,8 +2,8 @@
 
 require_relative '../lib/falafel'
 
-RSpec.describe 'Chomsky with epsilon' do
-  it 'Check chomsky normal form without epsilon' do
+RSpec.describe 'CYK implementing 0' do
+  it 'CYK word problem with epsilon' do
     alphabet  = %w[a b]
     vars_set  = %w[S]
     start_var = 'S'
@@ -25,8 +25,8 @@ RSpec.describe 'Chomsky with epsilon' do
   end
 end
 
-RSpec.describe 'CYK implementing' do
-  it 'CYK word problem' do
+RSpec.describe 'CYK implementing 1' do
+  it 'CYK word problem without any special cases' do
     alphabet  = %w[a b]
     vars_set  = %w[S]
     start_var = 'S'
@@ -47,7 +47,7 @@ RSpec.describe 'CYK implementing' do
 end
 
 RSpec.describe 'CYK implementing 2' do
-  it 'CYK word problem 2' do
+  it 'CYK word problem without any special cases 2' do
     alphabet  = %w[a b]
     vars_set  = %w[S X Y]
     start_var = 'S'
@@ -68,7 +68,7 @@ RSpec.describe 'CYK implementing 2' do
 end
 
 RSpec.describe 'CYK implementing 3' do
-  it 'CYK word problem 3' do
+  it 'CYK word problem with epsilon' do
     alphabet  = %w[a b c]
     vars_set  = ['S']
     start_var = 'S'
@@ -88,6 +88,32 @@ RSpec.describe 'CYK implementing 3' do
     check_results = { 'ac' => true,
                       'abcc' => true,
                       "#{'a' * x}#{'b' * y}#{'c' * z}" => true }
+    check_results.each do |word, res|
+      cfg.cyk_run word
+      result = cfg.is_in_l
+      expect(result).to eq(res)
+    end
+  end
+end
+
+RSpec.describe 'CYK implementing 4' do
+  it 'CYK word problem with epsilon (complicated)' do
+    alphabet  = %w[a b]
+    vars_set  = %w[S X Y]
+    start_var = 'S'
+    rules = { 'S' => [[], ['aSS'], ['SaY'], ['a']] }
+    rules['X'] = [['aX'], []]
+    rules['Y'] = [['bY'], []]
+
+    falafel = Falafel.new {}
+    cfg     = falafel.cfg alphabet, vars_set, start_var, rules
+
+    cfg.epsilon_free
+
+    cfg.chomsky_nf cfg.rules_ef
+
+    check_results = { 'aaa' => true, 'aab' => true }
+
     check_results.each do |word, res|
       cfg.cyk_run word
       result = cfg.is_in_l
